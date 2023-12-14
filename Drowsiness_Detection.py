5fe1f1d8eb68693ad4dee7203a983f7a4a2d1da2
Azhar Ali <121280275+azharali0@users.noreply.github.com>
2023-12-15 03:11:58 +0500

    ML Project

---
+++ Drowsiness_Detection.py
@@ -0,0 +1,66 @@
+from scipy.spatial import distance
+from imutils import face_utils
+from pygame import mixer
+import imutils
+import dlib
+import cv2
+
+
+mixer.init()
+mixer.music.load("music.wav")
+
+def eye_aspect_ratio(eye):# defines the ratio of an eye to be opened
+	A = distance.euclidean(eye[1], eye[5])#landmarks distance between the points
+	B = distance.euclidean(eye[2], eye[4])
+	C = distance.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
+	
+# imutils used facial landmark detection algorithm from the dlib
+thresh = 0.25
+frame_check = 20# Number of consecutive frames where the eyes are closed to trigger an alert.
+detect = dlib.get_frontal_face_detector()# used to detect the faces and landmarks in the images
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat") #  predicts 68 facial landmarks (points on the face, like eyes, nose, mouth, etc.)
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+cap=cv2.VideoCapture(0)
+flag=0
+while True:
+	ret, frame=cap.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	subjects = detect(gray, 0)
+	for subject in subjects:
+		shape = predict(gray, subject)
+		shape = face_utils.shape_to_np(shape)
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+		ear = (leftEAR + rightEAR) / 2.0
+		leftEyeHull = cv2.convexHull(leftEye)# convex for eyes detection
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)# contours for the blue shapes on eyes
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+		if ear < thresh:
+			flag += 1
+			print (flag)
+			if flag >= frame_check:
+				cv2.putText(frame, "****************ALERT!****************", (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				cv2.putText(frame, "****************ALERT!****************", (10,325),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				mixer.music.play()
+		else:
+			flag = 0
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+		break
+cv2.destroyAllWindows()
+cap.release() 
+
+
+
+
